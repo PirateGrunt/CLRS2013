edac2669a0e6763ab091e9ee68ef2a139931282e
BrianFannin <BFannin@RedwoodsGroup.com>
2013-08-09 10:59:59 -0400

    Explanatory comments

--- Install.R
+++ Install.R
@@ -1,5 +1,15 @@
-local = TRUE
+#' Ensure that the directory path uses Unix-style path separators and has a terminal separator.
+#' For example, my path would be as follows:
+#' localRoot = "C:/Users/bfannin/Documents/GitHub/CLRS2013/"
+#'
+#'
+#' Do not enter a path like:
+#' localRoot = "C:\Users\bfannin\Documents\GitHub\CLRS2013\"
+#' or
+#' localRoot = "C:/Users/bfannin/Documents/GitHub/CLRS2013"
+  
 localRoot = "Enter a valid directory path here"
+local = TRUE
 installPackages = FALSE
 
-source("https://raw.github.com/PirateGrunt/CLRS2013/")
+source("https://raw.github.com/PirateGrunt/CLRS2013/master/Startup.R")
